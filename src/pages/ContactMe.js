1e516dec8f3857a934a9fe81434f2259c2ebbd56
zyzzyxis <13ezimm@gmail.com>
2021-05-12 18:22:41 -0600

    contact form created, working on functionality

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -1,10 +1,58 @@
+import React from 'react';
+import { Button, Form , Container} from "react-bootstrap"
+import emailjs from 'emailjs-com';
+
+
 
 const ContactMe = () => {
+  
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('gmail', 'template_9rcijy7', e.target, 'user_hLwloleE2fK82EDvUyMPj')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    // e.target.reset()  
+  }
+
   return (
+    <>
     <div>
       <h1>CONTACT ME</h1>
-      <h3>Currently Under Construction <br/>Sorry for the mess!</h3>
+      <p>I can be contacted via <a target="_blank" href="https://www.linkedin.com/in/erin-zimmerman/">linkedin</a> currently.</p>
+     
     </div>
+      <Container>
+        <Form onSubmit={sendEmail}>
+          <Form.Group>
+            <Form.Control placeholder='Your Name' name='name' />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control placeholder='Your Email Address' name='email' />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control placeholder='Subject' name='subject' />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control placeholder='Message'  rows={5} type='textarea' as='textarea' name='message' />
+          </Form.Group>
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+            <Button type='submit' style={{background:"green", borderColor:"green", padding: '1em', width: '10em', fontWeight: 'bold'}}>
+              Send
+            </Button>
+          </div>
+        </Form>
+        {/* {emailSent && 
+          <div style={{display: 'flex', justifyContent: 'center', marginTop: '1em'}}>
+            <h5 style={{color: "green"}} >Email Sent!</h5>
+          </div>
+        } */}
+        </Container>
+      </>
+    
   )
 }
 
